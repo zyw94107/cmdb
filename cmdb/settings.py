f838e1050b6b877a4d264ee3575237185ae73d38
yiwei <zyw017@gmail.com>
2018-06-27 22:57:43 +0800

    add requirements

--- cmdb/settings.py
+++ cmdb/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'assets',
     'xadmin',
     'crispy_forms',
+    'reversion',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES = os.path.join(BASE_DIR, 'static')
